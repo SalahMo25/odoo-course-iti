d843d3d540d05dea1c87480105d450903939e494
salahiti <salahmohmed504@gmail.com>
2024-03-10 15:53:00 +0200

    lab1

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    'name': "Todo App",
+    'summary': """ """,
+    'description': """ """,
+    'author': "Salah Mohamed",
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+    'security/ir.model.access.csv',
+    'views/base_menus.xml',
+    'views/todo.xml',
+
+    ],
+}
